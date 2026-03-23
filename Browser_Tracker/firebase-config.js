2dc4cc9c621c32f77021c170cdf2d7e0802d365e
2Kaymoyo <wilbmoffitt@gmail.com>
2026-03-23 16:31:13 -0700

    Delete Browser_Tracker directory

--- Browser_Tracker/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase config for LocalTime Chrome Extension
-export const firebaseConfig = {
-  apiKey: "AIzaSyAhcVg8fJFugSjYHSTup9KHAsMDQiW49bo",
-  authDomain: "localtime-cloud-wm.firebaseapp.com",
-  projectId: "localtime-cloud-wm",
-  storageBucket: "localtime-cloud-wm.firebasestorage.app",
-  messagingSenderId: "173362342912",
-  appId: "1:173362342912:web:4e5e47899b69ef7c83bce5"
-};
